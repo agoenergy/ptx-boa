882d61d5e737a93c6241097fd35d0b51b1ed3fda
c.winger <c.winger@oeko.de>
2023-10-23 10:53:13 +0200

    dummy chain

--- ptxboa/api_chain.py
+++ ptxboa/api_chain.py
@@ -4,132 +4,10 @@
 import pandas as pd
 
 
-class ProcessMeta(type):
-    """Register all subclasses by process codes using metaclass."""
-
-    _classes = {}
-
-    def __init__(cls, name, bases, attrs):
-        super().__init__(name, bases, attrs)
-        for code in cls.codes:
-            assert code not in cls._classes  # no duplicates
-            # print(f"register {code} -> {cls.__name__}") # noqa
-            cls._classes[code] = cls
-
-    @classmethod
-    def get_process_class_by_code(cls, code: str):
-        """Get process class by code.
-
-        Parameters
-        ----------
-        code: str
-
-        Returns
-        -------
-        Process class
-        """
-        return cls._classes[code]
-
-    @classmethod
-    def get_process_by_code(cls, code: str, *args, **kwargs):
-        """Get process instance by code.
-
-        Parameters
-        ----------
-        code: str
-
-        Returns
-        -------
-        Process class instance
-        """
-        class_ = cls.get_process_class_by_code(code)
-        return class_(code, *args, **kwargs)
-
-
-class Process(metaclass=ProcessMeta):
-    codes = ()
-    flows = ()
-    parameters = ()
-    main_flow_in = None
-    main_flow_out = None
-    process_type = None
-    process_subtype = None
-
-    def __init__(self, code, get_parameter_value, subprocesses):
-        self.code = code
-        self.flow_procs = {}
-        self.param_values = {}
-
-        for p in self.parameters:
-            self.param_values[p] = get_parameter_value(self.code, p)
-
-        for flow in self.flows:
-            if flow in subprocesses:
-                self.flow_procs[flow] = Process.get_process_by_code(
-                    subprocesses[flow], get_parameter_value, None
-                )
-                # TODO: check that flow matches
-                # assert self.flow_procs[flow].main_flow_out == flow # noqa
-            else:
-                self.flow_procs[flow] = FlowMarketProcess(flow)
-
-    def _calculate_output_value(self, input_value) -> float:
-        return input_value * self.param_values["EFF"]
-
-    def _calculate_results(self, output_value) -> list:
-        return []
-
-    def __call__(self, input_value):
-        """Calculate results.
-
-        Parameter
-        ---------
-        input_value: float
-        """
-        output_value = self._calculate_output_value(input_value)
-
-        # adding tuple (process_type, process_subtype, cost_type, values)
-        results = self._calculate_results(output_value)
-
-        for fp in self.flow_procs.values():
-            _, results_ = fp(output_value)
-            results += results_
-
-        return output_value, results
-
-    def __str__(self):
-        return self.code
-
-
-class FlowMarketProcess:
-    """Dummy process to buy required flow at market cost."""
-
-    def __init__(self, flow, get_parameter_value):
-        self.main_flow_out = flow
-        self.param_speccost = get_parameter_value()
-
-
-class ProcessReGen(Process):
-    codes = ("PV-FIX", "PV-TRK", "RES-HYBR", "WIND-OFF", "WIND-ON")
-    flows = ()
-    parameters = ("EFF",)
-    main_flow_in = None
-    main_flow_out = "EL"
-    process_type = "Electricity generation"
-    process_subtype = "res"
-
-    def _calculate_results(self, output_value) -> list:
-        # DUMMY
-        return [
-            (self.process_type, self.process_subtype, "CAPEX", output_value * 0.1),
-            (self.process_type, self.process_subtype, "OPEX", output_value * 0.1),
-        ]
-
-
 class Chain:
     def __init__(self, get_parameter_value, process_codes, subprocesses):
         self.processes = [
-            Process.get_process_by_code(
+            GenericProcess.create_process(
                 code,
                 get_parameter_value=get_parameter_value,
                 subprocesses=subprocesses,
@@ -176,6 +54,164 @@ class Chain:
         return "->".join(str(p) for p in self.processes)
 
 
+class ProcessMeta(type):
+    """Register all subclasses by process codes using metaclass."""
+
+    _classes = {}
+
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+        for code in cls.codes:
+            assert code not in cls._classes  # no duplicates
+            # print(f"register {code} -> {cls.__name__}") # noqa
+            cls._classes[code] = cls
+
+    @classmethod
+    def create_process(cls, code: str, *args, **kwargs):
+        """Get process instance by code.
+
+        Parameters
+        ----------
+        code: str
+
+        Returns
+        -------
+        Process class instance
+        """
+        class_ = cls._classes[code]
+        return class_(code, *args, **kwargs)
+
+
+class GenericProcess(metaclass=ProcessMeta):
+    codes = ()
+    flows = ()
+    parameters = ()
+    main_flow_in = None
+    main_flow_out = None
+    process_type = None
+    process_subtype = None
+
+    def __init__(self, code, get_parameter_value, subprocesses):
+        self.code = code
+        self.flow_procs = {}
+        self.param_values = {}
+
+        for p in self.parameters:
+            self.param_values[p] = get_parameter_value(
+                process_code=self.code, parameter_code=p
+            )
+
+        for flow in self.flows:
+            if flow in subprocesses:
+                self.flow_procs[flow] = ProcessSecondary.create_process(
+                    self, subprocesses[flow], get_parameter_value
+                )
+                # TODO: check that flow matches
+                # assert self.flow_procs[flow].main_flow_out == flow # noqa
+            else:
+                self.flow_procs[flow] = ProcessSecondaryMarket(
+                    self, flow, get_parameter_value
+                )
+
+    def _calculate_output_value(self, input_value) -> float:
+        return input_value
+
+    def _calculate_results(self, output_value) -> list:
+        return []
+
+    def __call__(self, input_value):
+        """Calculate results.
+
+        Parameter
+        ---------
+        input_value: float
+        """
+        output_value = self._calculate_output_value(input_value)
+
+        # adding tuple (process_type, process_subtype, cost_type, values)
+        results = self._calculate_results(output_value)
+
+        for fp in self.flow_procs.values():
+            _, results_ = fp(output_value)
+            results += results_
+
+        return output_value, results
+
+    def __str__(self):
+        return self.code
+
+
+class ProcessMain(GenericProcess):
+    """Process in main chain."""
+
+    parameters = ("EFF",)
+
+    def _calculate_results(self, output_value) -> list:
+        results = super()._calculate_results(output_value)
+        # DUMMY
+        results += [
+            (self.process_type, self.process_subtype, "CAPEX", output_value * 0.1),
+            (self.process_type, self.process_subtype, "OPEX", output_value * 0.1),
+        ]
+        return results
+
+
+class ProcessSecondary(GenericProcess):
+    """Secondary process to generate required flows."""
+
+    parameters = ("CONV",)
+
+    def __init__(self, parent_process, code, get_parameter_value):
+        super().__init__(code, get_parameter_value, subprocesse={})
+        self.parent_process = parent_process
+
+
+class ProcessSecondaryMarket(ProcessSecondary):
+    """Dummy process to buy required flows at market cost."""
+
+    def __init__(self, parent_process, flow: str, get_parameter_value):
+        self.code = ""
+        self.process_type = "TODO"
+        self.process_subtype = "TODO"
+        self.flow_procs = {}
+        self.parent_process = parent_process
+        self.main_flow_out = flow
+        self.param_values = {
+            "SPECCOST": get_parameter_value(flow=flow, parameter="SPECCOST")
+        }
+
+    def _calculate_results(self, output_value) -> list:
+        results = super()._calculate_results(output_value)
+        results += [
+            (
+                self.process_type,
+                self.process_subtype,
+                "FLOW",
+                output_value * self.param_values["SPECCOST"],
+            ),
+        ]
+        return results
+
+
+class ProcessReGen(ProcessMain):
+    codes = ("PV-FIX", "PV-TRK", "RES-HYBR", "WIND-OFF", "WIND-ON")
+    main_flow_out = "EL"
+    process_type = "Electricity generation"
+    process_subtype = "res"
+
+    def _calculate_output_value(self, input_value) -> float:
+        return input_value * self.param_values["EFF"]
+
+
+class ProcessELEC(ProcessMain):
+    codes = ("AEL-EL", "PEM-EL", "SOEC-EL")
+    main_flow_in = "EL"
+    main_flow_out = "H2-G"
+    process_type = "Electrolysis"
+    process_subtype = "ely"
+    flows = ("CO2-G",)
+
+
 if __name__ == "__main__":
     # Dummy implementation
 
@@ -184,7 +220,7 @@ if __name__ == "__main__":
         return 1
 
     # setup chain
-    chain = Chain(get_parameter_value, ["WIND-ON"], {"H2O-L": "DESAL"})
+    chain = Chain(get_parameter_value, ["WIND-ON", "AEL-EL"], {"H2O-L": "DESAL"})
 
     print(chain)
 
