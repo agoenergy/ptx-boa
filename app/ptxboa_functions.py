b739aedb14cdb75081598123cffc7886a21183a9
Markus Haller <m.haller@oeko.de>
2023-11-10 11:50:08 +0100

    add help text to certification sheet #58

--- app/ptxboa_functions.py
+++ app/ptxboa_functions.py
@@ -1002,7 +1002,7 @@ def create_fact_sheet_certification_schemes(context_data: dict):
     with st.expander("What is this?"):
         st.markdown(
             """
-**Get supplementary information on H2-relevant certification frameworks **
+**Get supplementary information on H2-relevant certification frameworks**
 
 This sheet provides you with an overview of current governmental regulations
 and voluntary standards for H2 products.
@@ -1028,6 +1028,23 @@ and voluntary standards for H2 products.
         st.markdown(f"- **PTXBOA demand countries:** {data['ptxboa_demand_countries']}")
         st.markdown(f"- **Labels:** {data['label']}")
         st.markdown(f"- **Lifecycle scope:** {data['lifecycle_scope']}")
+
+        st.markdown(
+            """
+**Explanations:**
+
+- Info on "Geographical scope":
+  - This field provides an answer to the question: if you want to address a specific
+ country of demand, which regulations and/or standards exist in this country
+   that require or allow proof of a specific product property?
+- Info on "Lifecycle scope":
+  - Well-to-gate: GHG emissions are calculated up to production.
+  - Well-to-wheel: GHG emissions are calculated up to the time of use.
+  - Further information on the life cycle scopes can be found in
+IRENA & RMI (2023): Creating a global hydrogen market: certification to enable trade,
+ pp. 15-19
+"""
+        )
 
     with st.expander("**Scope**"):
         if data["scope_emissions"] != "not specified":
