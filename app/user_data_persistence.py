575621a18e0f25cff610805842fa974fc2275928
j.aschauer <j.aschauer@oeko.de>
2023-11-28 17:12:08 +0100

    improve used data upload
    - switch to csv
    - validate index combinations in uploaded data
    - remove uploaded file when applied

--- app/user_data_persistence.py
+++ app/user_data_persistence.py
@@ -7,12 +7,16 @@ import streamlit as st
 
 def upload_user_data(api):
     if st.session_state["edit_input_data"]:
+        if "file_uploader_key" not in st.session_state:
+            st.session_state["file_uploader_key"] = 0
+
         uploaded_file = st.file_uploader(
             label="modified data file",
-            type="json",
+            type="csv",
             accept_multiple_files=False,
             help="Select modified data file downloaded from a previous session.",
             label_visibility="collapsed",
+            key=st.session_state["file_uploader_key"],
         )
         if uploaded_file is not None:
             validated = validate_uploaded_user_data(api, uploaded_file)
@@ -20,10 +24,13 @@ def upload_user_data(api):
             validated = None
 
         if isinstance(validated, pd.DataFrame):
+            st.success("Valid data file.")
             if st.session_state["user_changes_df"] is not None:
-                st.warning("Apply settings will override your current changes.")
+                st.warning(
+                    "Applying data loaded from file will override your current changes."
+                )
             st.button(
-                label="Apply Settings",
+                label="Apply uploaded data",
                 on_click=apply_uploaded_user_data,
                 args=(validated,),
             )
@@ -32,14 +39,14 @@ def upload_user_data(api):
             st.error(f"Uploaded data is not valid: {validated}")
 
         if not isinstance(validated, pd.DataFrame) or uploaded_file is None:
-            st.warning("Select data to be uploaded.")
+            st.warning("Select data file to be uploaded.")
 
 
 def validate_uploaded_user_data(api, uploaded_file):
     try:
-        result = pd.read_json(uploaded_file, orient="records")
-    except ValueError:
-        return "json parsing error"
+        result = pd.read_csv(uploaded_file, keep_default_na=False)
+    except:  # noqa
+        return "csv parsing error"
 
     if set(result.columns) != {
         "source_region_code",
@@ -50,27 +57,34 @@ def validate_uploaded_user_data(api, uploaded_file):
         return "wrong column names"
 
     # check that index column combination is present in input data:
-    api.get_input_data(
-        st.session_state["scenario"],
-    )
-    # TODO: validate records in result
+    input_data = api.get_input_data(st.session_state["scenario"], long_names=True)
+    for row in result.itertuples():
+        selector = (
+            (input_data["parameter_code"] == row.parameter_code)
+            & (input_data["process_code"] == row.process_code)
+            & (input_data["flow_code"] == "")
+            & (input_data["source_region_code"] == row.source_region_code)
+            & (input_data["target_country_code"] == "")
+        )
+        if len(input_data.loc[selector]) == 0:
+            return (
+                f"invalid index combination '{row.source_region_code} "
+                f"| {row.process_code} | {row.parameter_code}'"
+            )
 
     return result.replace("", None)
 
 
 def download_user_data():
-    data = (
-        st.session_state["user_changes_df"]
-        .fillna("")
-        .to_json(orient="records", indent=2)
-    )
+    data = st.session_state["user_changes_df"].fillna("").to_csv(index=False)
     st.download_button(
         label="Download Modified Data",
         data=data,
-        file_name="modified_ptxboa_data.json",
+        file_name="modified_ptxboa_data.csv",
         help="Click to download your current data modifications.",
     )
 
 
 def apply_uploaded_user_data(uploaded_df):
     st.session_state["user_changes_df"] = uploaded_df
+    st.session_state["file_uploader_key"] += 1
