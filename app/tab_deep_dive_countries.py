a8b7d07453feb88f28eeaf2cbc36fa54b9f178d0
j.aschauer <j.aschauer@oeko.de>
2023-12-07 16:43:10 +0100

    improve layout with borderd containers

--- app/tab_deep_dive_countries.py
+++ app/tab_deep_dive_countries.py
@@ -42,54 +42,58 @@ They also show the data for your selected supply country or region for compariso
         "Select country:", ["Argentina", "Morocco", "South Africa"], horizontal=True
     )
 
-    fig_map = plot_costs_on_map(
-        api, costs_per_region, scope=ddc, cost_component="Total"
-    )
-    st.plotly_chart(fig_map, use_container_width=True)
-
-    display_costs(
-        select_subregions(costs_per_region, ddc),
-        key="region",
-        titlestring="Costs per subregion",
-        key_suffix=ddc,
-    )
-
-    st.subheader("Full load hours of renewable generation")
-
-    # in order to keep the figures horizontally aligned, we create two st.columns pairs
-    # the columns are identified by c_{row}_{column}, zero indexed
-    c_0_0, c_0_1 = st.columns([2, 1], gap="large")
-    c_1_0, c_1_1 = st.columns([2, 1], gap="large")
-    with c_0_0:
-        st.markdown("**Map**")
-        map_parameter = st.selectbox(
-            "Show Parameter on Map",
-            [
-                "Wind Onshore",
-                "Wind Offshore",
-                "PV tilted",
-                "Wind-PV-Hybrid",
-            ],
-            key="ddc_flh_map_parameter",
+    with st.container(border=True):
+        fig_map = plot_costs_on_map(
+            api, costs_per_region, scope=ddc, cost_component="Total"
         )
-    with c_1_0:
-        fig = plot_input_data_on_map(
-            api=api,
-            data_type="full load hours",
-            color_col=map_parameter,
-            scope=ddc,
-        )
-        st.plotly_chart(fig, use_container_width=True)
-    with st.expander("**Data**"):
-        df = display_and_edit_input_data(
-            api,
-            data_type="full load hours",
-            scope=ddc,
-            key="input_data_editor_full_load_hours_ddc",
+        st.plotly_chart(fig_map, use_container_width=True)
+
+        st.divider()
+
+        display_costs(
+            select_subregions(costs_per_region, ddc),
+            key="region",
+            titlestring="Costs per subregion",
+            key_suffix=ddc,
         )
 
-    with c_0_1:
-        st.markdown("**Regional Distribution**")
-    with c_1_1:
-        fig = px.box(df)
-        st.plotly_chart(fig, use_container_width=True)
+    with st.container(border=True):
+        st.subheader("Full load hours of renewable generation")
+
+        # in order to keep the figures horizontally aligned, we create two st.columns
+        # pairs, the columns are identified by c_{row}_{column}, zero indexed
+        c_0_0, c_0_1 = st.columns([2, 1], gap="large")
+        c_1_0, c_1_1 = st.columns([2, 1], gap="large")
+        with c_0_0:
+            st.markdown("**Map**")
+            map_parameter = st.selectbox(
+                "Show Parameter on Map",
+                [
+                    "Wind Onshore",
+                    "Wind Offshore",
+                    "PV tilted",
+                    "Wind-PV-Hybrid",
+                ],
+                key="ddc_flh_map_parameter",
+            )
+        with c_1_0:
+            fig = plot_input_data_on_map(
+                api=api,
+                data_type="full load hours",
+                color_col=map_parameter,
+                scope=ddc,
+            )
+            st.plotly_chart(fig, use_container_width=True)
+        with st.expander("**Data**"):
+            df = display_and_edit_input_data(
+                api,
+                data_type="full load hours",
+                scope=ddc,
+                key="input_data_editor_full_load_hours_ddc",
+            )
+
+        with c_0_1:
+            st.markdown("**Regional Distribution**")
+        with c_1_1:
+            fig = px.box(df)
+            st.plotly_chart(fig, use_container_width=True)
