5569757901ea5de1773962bea32c91ccc14e086c
Markus Haller <m.haller@oeko.de>
2023-12-11 16:20:41 +0100

    move markdown text to .md file

--- app/tab_sustainability.py
+++ app/tab_sustainability.py
@@ -2,6 +2,8 @@
 """Content of sustainability tab."""
 import streamlit as st
 
+from app.ptxboa_functions import read_markdown_file
+
 
 def _render_figure_and_introduction():
     st.image("static/sustainability.png")
@@ -10,35 +12,7 @@ def _render_figure_and_introduction():
         "PtX-Hub-PtX.Sustainability-Dimensions-and-Concerns-Scoping-Paper.pdf"
     )
     st.caption(captiontext)
-
-    st.markdown(
-        """
-**What sustainability aspects should be considered for PTX products,
-production and policies?**
-
-**What questions should be asked before and during project development?**
-
-In this tab we aim to provide a basic approach to these questions.
-The diagram shows the framework along which the compilation
-of sustainability aspects in this tab is structured. It is based on the EESG framework
-as elaborated by the PtX Hub and sustainability criteria developed by the Öko-Institut.
-
-**The framework distinguishes four key sustainability dimensions - Environmental,
-Economic, Social and Governance - from which you can select below.**
-
-Within each of these dimensions there are different clusters of sustainability aspects
-that we address in a set of questions. We differentiate between questions indicating
-guardrails and questions suggesting goals.
-
-With this compilation, we aim to provide a general overview of the sustainability
-issues that may be relevant in the context of PTX production. Of course,
-different aspects are more or less important depending on the project,
-product and country.
-
-**Take a look for yourself to see which dimensions are most important
-from where you are coming from.**
-                    """
-    )
+    st.markdown(read_markdown_file("static/sustainability_intro.md"))
 
 
 def _interactive_sustainability_dimension_info(context_data: dict):
