04da604bfb684c87a40edb90422e954bdcafc24d
Markus Haller <m.haller@oeko.de>
2023-12-11 17:18:20 +0100

    add logos to info tab

--- app/tab_info.py
+++ app/tab_info.py
@@ -6,6 +6,8 @@ from app.ptxboa_functions import read_markdown_file
 
 
 def content_info():
+    st.image("img/logos.png")
+
     with st.container(border=True):
         st.markdown(read_markdown_file("md/info_intro.md"))
 
