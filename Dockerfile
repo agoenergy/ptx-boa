380fb2ebb813cf098effcad0c8d42bf661f148d2
c.winger <c.winger@oeko.de>
2024-01-15 14:00:39 +0100

    Bump version: 0.1.0 → 0.1.1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.10-slim
-LABEL version="0.1.0"
+LABEL version="0.1.1"
 
 RUN apt-get update
 RUN apt install -y git
--- ptxboa_streamlit.py
+++ ptxboa_streamlit.py
@@ -6,7 +6,7 @@ Execution:
 >>> streamlit run  ptxboa_streamlit.py
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # TODO how do I use the treamlit logger?
 import logging
