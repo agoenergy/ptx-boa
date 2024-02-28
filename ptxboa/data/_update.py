f4f5eaf73d2a75b0a409dccdd87aa3fcb57ad1bb
j.aschauer <j.aschauer@oeko.de>
2024-02-28 15:53:56 +0100

    move update of static data files to _update static

--- ptxboa/data/_update.py
+++ ptxboa/data/_update.py
@@ -21,96 +21,11 @@ def update_csv(query: str, filename: str, data_dir: str = None) -> None:
 
 
 def main():
-    update_csv(
-        """
-        SELECT
-        "chain"
-        ,"ELY"
-        ,"DERIV"
-        ,"PRE_SHP"
-        ,"PRE_PPL"
-        ,"POST_SHP"
-        ,"POST_PPL"
-        ,"SHP"
-        ,"SHP-OWN"
-        ,"PPLS"
-        ,"PPL"
-        ,"PPLX"
-        ,"PPLR"
-        ,"FLOW_OUT"
-        ,"CAN_PIPELINE"
-        FROM "ptxboa_chains"
-        ORDER BY "chain"
-        """,
-        "chains.csv",
-    )
 
     update_csv(
         """
         SELECT
-        "parameter_code",
-        "parameter_name",
-        "unit",
-        "per_flow",
-        "per_transformation_process",
-        "per_transport_process",
-        "per_re_generation_process",
-        "per_process",
-        "per_region",
-        "per_import_country",
-        "has_global_default",
-        "global_default_changeable",
-        "own_country_changeable",
-        "comment",
-        "dimensions"
-        FROM "ptxboa_parameter"
-        ORDER BY "parameter_code"
-        """,
-        "dim_parameter.csv",
-    )
-
-    update_csv(
-        """
-        SELECT
-         "process_code"
-        ,"process_name"
-        ,"main_flow_code_out"
-        ,"main_flow_code_in"
-        ,"is_transformation"
-        ,"is_re_generation"
-        ,"is_transport"
-        ,"is_secondary"
-        ,"process_class"
-        /*,"is_secondary_all"*/
-        ,"is_ely"
-        ,"is_deriv"
-        /*,"class_name"*/
-        ,"result_process_type"
-        ,"secondary_flows"
-        FROM "ptxboa_process"
-        ORDER BY "process_code"
-        """,
-        "dim_process.csv",
-    )
-
-    update_csv(
-        """
-        SELECT
-        "flow_code",
-        "flow_name",
-        "unit",
-        "secondary_process",
-        "secondary_flow",
-        "result_process_type"
-        FROM "ptxboa_flow"
-        ORDER BY "flow_code"
-        """,
-        "dim_flow.csv",
-    )
-
-    update_csv(
-        """
-        SELECT
+        "key",
         "region",
         "process_res",
         "process_ely",
@@ -127,6 +42,7 @@ def main():
     update_csv(
         """
         select
+        "key",
         "process_res",
         "process_ely",
         "process_deriv",
@@ -144,6 +60,7 @@ def main():
             update_csv(
                 f"""
                 select
+                "key",
                 "parameter_code",
                 "process_code",
                 "flow_code",
