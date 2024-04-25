b08b20e7806ebcc24cd52d7d704cb82a9e2ca78b
j.aschauer <j.aschauer@oeko.de>
2024-04-25 15:47:24 +0200

    add solver status from metadata, add network reading

--- flh_opt/collect_cached_optimization_results.py
+++ flh_opt/collect_cached_optimization_results.py
@@ -9,6 +9,7 @@ Iterates over cache files and writes two files to outdir:
 """
 
 import argparse
+import json
 import logging
 import pickle  # noqa S403
 import sys
@@ -16,6 +17,7 @@ from pathlib import Path
 from typing import Literal
 
 import pandas as pd
+import pypsa
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from ptxboa import DEFAULT_CACHE_DIR
@@ -41,14 +43,40 @@ def main(
         raise FileNotFoundError(f"cache_dir doe not exists {cache_dir}")
 
     results = []
+    network_statistics = []
     for filepath in cache_dir.rglob("*.pickle"):
-        with filepath.open("rb") as file:
-            result = pickle.load(file)  # noqa S301
-            results.append(result)
+        f_hash = f"{filepath.parents[1].name}{filepath.parent.name}{filepath.stem}"
+        # this is the respective network object filepath
+        network_path = filepath.parent / f"{filepath.name}.network.nc"
+        # this is the metadata filepath
+        metadata_path = filepath.parent / f"{filepath.name}.metadata.json"
+        with filepath.open("rb") as pfile:
+            result = pickle.load(pfile)  # noqa S301
+
+        try:
+            with metadata_path.open("r", encoding="utf-8") as mfile:
+                metadata = json.load(mfile)
+            result["model_status"] = metadata["model_status"]
+        except FileNotFoundError:
+            result["model_status"] = "NaN"
+
+        try:
+            network = pypsa.Network()
+            network.import_from_netcdf(network_path)
+            network_statistics.append(network.statistics())
+        except (FileNotFoundError, ValueError) as e:
+            logging.error(f"Error for Network {network_path}: {e}")
+            pass
+
+        result["optimization_hash"] = f_hash
+
+        results.append(result)
 
     # extract record in "main_process_chain"
     main_process_chain = pd.json_normalize(
-        results, record_path=["main_process_chain"], meta="context"
+        results,
+        record_path=["main_process_chain"],
+        meta=["context", "optimization_hash", "model_status"],
     )
     # normalize entries in "context" to single columns
     main_process_chain = pd.concat(
@@ -69,6 +97,10 @@ def main(
         outdir / "cached_optimization_data_main_process_chain.csv"
     )
     secondary_process.to_csv(outdir / "cached_optimization_data_secondary_process.csv")
+
+    if len(network_statistics) > 0:
+        network_statistics = pd.concat(network_statistics)
+        network_statistics.to_csv(outdir / "network_statistics.csv")
 
     return main_process_chain
 
