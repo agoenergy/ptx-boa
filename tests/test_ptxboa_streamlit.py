f0905f74f2700db524894501a66cfe7f6d51b7b6
j.aschauer <j.aschauer@oeko.de>
2024-05-15 11:40:28 +0200

    add smoke test for each tab individually

--- tests/test_ptxboa_streamlit.py
+++ tests/test_ptxboa_streamlit.py
@@ -16,3 +16,30 @@ def running_app():
 def test_app_smoke(running_app):
     """Test if the app starts up without errors."""
     assert not running_app.exception
+
+
+@pytest.fixture(
+    params=(
+        "Info",
+        "Costs",
+        "Market scanning",
+        "Input data",
+        "Deep-dive countries",
+        "Country fact sheets",
+        "Certification schemes",
+        "Sustainability",
+        "Literature",
+        "Optimization",
+    )
+)
+def running_app_on_tab(request):
+    tab = request.param
+    at = AppTest.from_file("ptxboa_streamlit.py")
+    at.session_state["tab_key"] = "tab_key_0"
+    at.session_state[at.session_state["tab_key"]] = tab
+    at.run(timeout=60)
+    return at
+
+
+def test_tabs_smoke(running_app_on_tab):
+    assert not running_app_on_tab.exception
