08e93117cce347dd76f0327c9ef02c4ab449c42a
c.winger <c.winger@oeko.de>
2026-03-11 16:48:51 +0100

    update test data

--- tests/test_blue.py
+++ tests/test_blue.py
@@ -5,8 +5,7 @@ from pprint import pprint
 import pandas as pd
 import pytest
 
-from ptxboa import DEFAULT_DATA_DIR
-from ptxboa.api import PtxboaAPI, PtxCalc
+from ptxboa.api import PtxCalc
 from ptxboa.api_data import DataHandler
 from tests.test_api import ptxdata_dir_static
 
@@ -120,7 +119,7 @@ def test_new_blue_chain(scenario, kwargs):
                 "process_code": "NG-DRI-C#B",
                 "flow_code": "NG-G",  # main flow in
                 "parameter_code": "LOSS",
-                "value": 0.05,  # process_calc!E7
+                "value": 0.05,  # process_calc!E9
             },
             {
                 "process_code": "NG-DRI-C#B",
@@ -137,25 +136,25 @@ def test_new_blue_chain(scenario, kwargs):
                 "process_code": "NG-DRI-C#B",
                 "flow_code": "IOP-S",
                 "parameter_code": "CONV",
-                "value": 0.99,  # process_calc!E5
+                "value": 1.373737,  # process_calc!E6
             },
             {
                 "process_code": "NG-DRI-C#B",
                 "flow_code": "HEAT",
                 "parameter_code": "CONV",
-                "value": 0.01,  # process_calc!E6
+                "value": 0.01,  # process_calc!E7
             },
             {
                 "process_code": "EAF#B",
                 "flow_code": "HEAT",
                 "parameter_code": "CONV",
-                "value": 0.01,  # process_calc!J6
+                "value": 0.01,  # process_calc!J7
             },
             {
                 "process_code": "EAF#B",
                 "parameter_code": "LOSS",
                 "flow_code": "NG-G",
-                "value": 0.05,  # process_calc!J7
+                "value": 0.05,  # process_calc!J9
             },
             {
                 "process_code": "EAF#B",
@@ -166,38 +165,32 @@ def test_new_blue_chain(scenario, kwargs):
                 "process_code": "EAF#B",
                 "flow_code": "EL",
                 "parameter_code": "CONV",
-                "value": 0.651000,  # process_calc!J5
-            },
-            {
-                "process_code": "EAF#B",
-                "flow_code": "NG-G",
-                "parameter_code": "CONV",
-                "value": 0.3,  # process_calc!J4
+                "value": 0.651000,  # process_calc!J6
             },
             {
                 "process_code": "NG-DRI-C#B",
                 "parameter_code": "CO2CPT-S",
-                "value": 0.45,  # process_calc!E10
+                "value": 0.45,  # process_calc!E11
             },
             {
                 "process_code": "NG-DRI-C#B",
                 "parameter_code": "CO2CPT-R",
-                "value": 0.9,  # process_calc!E13
+                "value": 0.9,  # process_calc!E14
             },
             {
                 "flow_code": "NG-G",
                 "parameter_code": "CO2BOUND",
-                "value": 0.0408116143367898,  # process_calc!E16
+                "value": 0.040812,  # process_calc!E17
             },
             {
                 "flow_code": "NG-G",
                 "parameter_code": "CH4SHARE",
-                "value": 0.909,  # process_calc!E18
+                "value": 0.909,  # process_calc!E20
             },
             {
                 "flow_code": "STL-S",
                 "parameter_code": "CO2BOUND",
-                "value": 0.00396,  # process_calc!J16
+                "value": 0.00396,  # process_calc!J17
             },
             {
                 "parameter_code": "EF_M",
@@ -206,27 +199,38 @@ def test_new_blue_chain(scenario, kwargs):
             },
             {
                 "parameter_code": "EF_M",
+                "flow_code": "CH4-G",
+                "value": 201,  # emission_factors!I7
+            },
+            {
+                "parameter_code": "EF_E",
+                "flow_code": "NG-G",
+                "source_region_code": "QAT",
+                "value": 201,  # emission_factors!D9
+            },
+            {
+                "parameter_code": "EF_E",
                 "flow_code": "EL",
                 "source_region_code": "QAT",
-                "value": 402,  # emission_factors!E7
+                "value": 402,  # emission_factors!E9
             },
             {
-                "parameter_code": "EF_M",
+                "parameter_code": "EF_E",
+                "flow_code": "EL",
+                # "target_country_code": "DEU", # noqa
+                "value": 300,  # emission_factors!F9
+            },
+            {
+                "parameter_code": "EF_E",
                 "flow_code": "HEAT",
                 "source_region_code": "QAT",
-                "value": 250,  # emission_factors!G7
+                "value": 250,  # emission_factors!G9
             },
             {
-                "parameter_code": "EF_M",
-                "flow_code": "EL",
-                "target_country_code": "DEU",
-                "value": 300,  # emission_factors!F7
-            },
-            {
-                "parameter_code": "EF_M",
+                "parameter_code": "EF_E",
                 "flow_code": "HEAT",
-                "target_country_code": "DEU",
-                "value": 250,  # emission_factors!H7
+                # "target_country_code": "DEU", # noqa
+                "value": 250,  # emission_factors!H9
             },
         ]
     )
@@ -258,7 +262,7 @@ def test_new_blue_chain(scenario, kwargs):
                 "CO2BOUND": {"NG-G": 0.040812},
                 "CO2CPT-R": 0.9,
                 "CO2CPT-S": 0.45,
-                "CONV": {"EL": 0.476859, "IOP-S": 0.99},
+                "CONV": {"EL": 0.476859, "IOP-S": 1.373737},
                 "EFF": 0.320004,
                 "EF_M": {"EL": 402.0, "NG-G": 201.0},
                 "FLH": 7000,
@@ -275,7 +279,7 @@ def test_new_blue_chain(scenario, kwargs):
                 "CAPEX": 0,
                 "CH4SHARE": {"NG-G": 0.909},
                 "CO2BOUND": {"NG-G": 0.040812, "STL-S": 0.00396},
-                "CONV": {"EL": 0.651, "NG-G": 0.315},
+                "CONV": {"EL": 0.651},
                 "EFF": 1.010101,
                 "EF_M": {"EL": 402.0, "NG-G": 201.0},
                 "FLH": 7000,
@@ -296,7 +300,6 @@ def test_new_blue_chain(scenario, kwargs):
                 "HEAT": 0.0577,
                 "IOP-S": 0.0,
                 "N2-G": 0.01154,
-                "NG-G": 0.0,
             },
             "WACC": 0.0,
         },
@@ -322,15 +325,18 @@ def test_new_blue_chain(scenario, kwargs):
         {
             "emissions": {
                 "co2_bound_output": 0.0,
-                "co2_captured": -0.0487,
+                "co2_captured": -0.048701,
                 "co2_emission_direct": 0.006012,
-                "co2_emission_from_bound": -0.071547,
-                "co2_indirect": 189.780347,
+                "co2_emission_from_bound": -0.071548,
+                "co2_indirect": 189.780347,  # E62
                 "co2e_emission_direct": 3.996637,
             },
-            "flows": {"EL": 0.47209, "IOP-S": 0.9801},
-            "main_input": 3.093713,
-            "main_output": 0.99,
+            "flows": {
+                "EL": 0.47209,  # E30
+                "IOP-S": 1.36,  # E32
+            },
+            "main_input": 3.093713,  # E28
+            "main_output": 0.99,  # E39
             "process_code": "NG-DRI-C#B",
             "process_step": "DERIV",
         },
@@ -344,45 +350,24 @@ def test_new_blue_chain(scenario, kwargs):
                 "co2e_emission_direct": 0.0,
             },
             "flows": {},
-            "main_input": 0.99,
-            "main_output": 0.99,
+            "main_input": 0.99,  # E39
+            "main_output": 0.99,  # E39
             "process_code": "DRI-SB#B",
             "process_step": "SHP",
         },
         {
             "emissions": {
                 "co2_bound_output": 0.00396,
-                "co2_captured": -0.0,
-                "co2_emission_direct": 0.000612,
-                "co2_emission_from_bound": -0.008283,
-                "co2_indirect": 325.017,
-                "co2e_emission_direct": 0.000612,
+                "co2_captured": 0.0,
+                "co2_emission_direct": 0.0,
+                "co2_emission_from_bound": 0.00396,
+                "co2_indirect": 261.702,  # J62
+                "co2e_emission_direct": 0.0,
             },
-            "flows": {"EL": 0.651, "NG-G": 0.315},
-            "main_input": 0.99,
-            "main_output": 1.0,
+            "flows": {"EL": 0.651},  # J32
+            "main_input": 0.99,  # E39
+            "main_output": 1.0,  # J39
             "process_code": "EAF#B",
             "process_step": "DERIV_I",
         },
     ]
-
-
-if __name__ == "__main__":
-    # TODO: convert into test function after
-    api = PtxboaAPI(data_dir=DEFAULT_DATA_DIR)
-
-    res = api.calculate(
-        scenario="2030 (medium)",
-        secproc_co2=None,
-        secproc_water=None,
-        chain="STL-S__NG-DRI-C-C_EAF__prod_in_demand",
-        res_gen=None,
-        region="Qatar",
-        country="Germany",
-        transport="Ship",
-        ship_own_fuel=False,
-        tool_version_color="blue",
-    )
-
-    for x in res.todo_results_flows:
-        pprint(_sort_nested(_round_nested(x)))
