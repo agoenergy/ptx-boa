5bb67ed0756268483f200ad6d576611c84cda0b4
c.winger <c.winger@oeko.de>
2026-03-23 11:06:27 +0100

    Bump version: 2.1.27 → 2.1.28

--- app/tab_green_info.py
+++ app/tab_green_info.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 from app.ptxboa_functions import read_markdown_file
 
-__version__ = "2.1.27"
+__version__ = "2.1.28"
 
 
 def content_info():
--- ptxboa_streamlit.py
+++ ptxboa_streamlit.py
@@ -6,7 +6,7 @@ Execution:
 >>> streamlit run  ptxboa_streamlit.py
 """
 
-__version__ = "2.1.27"
+__version__ = "2.1.28"
 
 import logging
 
