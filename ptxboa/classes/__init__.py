fd6a88180f7097e33f7d3f5270d64402659a4ca0
c.winger <c.winger@oeko.de>
2026-03-26 12:33:03 +0100

    addbase classes

---
+++ ptxboa/classes/__init__.py
@@ -0,0 +1,85 @@
+"""Base classes and class factories."""
+
+from dataclasses import dataclass
+from typing import ClassVar, Optional, cast
+
+from typing_extensions import Self  # for python 3.11+: from typing import Self
+
+
+class AbstractBase:
+    @classmethod
+    def _find_subclass_by_name(cls, class_name: str) -> type[Self]:
+        if cls.__name__ == class_name:
+            return cls
+        for subclass in cls.__subclasses__():
+            try:
+                return subclass._find_subclass_by_name(class_name=class_name)
+            except NotImplementedError:
+                pass
+        raise NotImplementedError(class_name)
+
+    @classmethod
+    def _create_subclass(
+        cls, class_name: str, template_class_name: str | None = None, **class_attrs
+    ) -> type[Self]:
+
+        base_class = (
+            cls._find_subclass_by_name(template_class_name)
+            if template_class_name
+            else cls
+        )
+
+        # Create new class
+        new_class = type(class_name, (base_class,), class_attrs)
+
+        # Apply dataclass decorator
+        # new_cls = dataclass(new_cls, frozen=True, slots=True) # noqa: E800
+
+        # type cast for IDE hints
+        new_class = cast(type[Self], new_class)
+
+        return new_class
+
+    @property
+    def classname(self) -> str:
+        """Class name."""
+        return self.__class__.__name__
+
+
+@dataclass(frozen=True, slots=True)
+class PtxboaBase(AbstractBase):
+    """Project base class."""
+
+    _code: ClassVar[str]
+    _name: ClassVar[str]
+
+    @property
+    def name(self) -> str:
+        """Human readable name."""
+        return self._name
+
+    @property
+    def code(self) -> str:
+        """Internal database code."""
+        return self._code
+
+
+@dataclass(frozen=True, slots=True)
+class PtxboaValue(PtxboaBase):
+    """Single value container."""
+
+    value: Optional[float] = 0
+
+
+@dataclass(frozen=True, slots=True)
+class PtxboaFlow(PtxboaValue):
+    """Flow variable."""
+
+    pass
+
+
+@dataclass(frozen=True, slots=True)
+class PtxboaParameter(PtxboaValue):
+    """Input Parameter."""
+
+    pass
