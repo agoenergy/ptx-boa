50aaae1b844401813fb86703d0c7d6885f6cf740
Christian Winger <c@wingechr.de>
2026-03-27 07:54:37 +0100

    WIP classes

--- ptxboa/classes/base.py
+++ ptxboa/classes/base.py
@@ -79,6 +79,12 @@ class PtxboaProcess(PtxboaInstanceBase):
 
     def __post_init__(self):
 
+        # check
+        if self.dtype.main_flow_type_out != self.main_flow_out.dtype:
+            raise TypeError(
+                f"{self.dtype.main_flow_type_out} != {self.main_flow_out.dtype}"
+            )
+
         main_flow_in = self.dtype.main_flow_type_in.create(
             self.main_flow_out.value / self.eff.value
         )
@@ -102,10 +108,6 @@ class PtxboaProcessType(PtxboaTypeBase):
 
     def create(self, get_data: Callable, main_flow_out: "PtxboaFlow") -> PtxboaProcess:
         """Create instance."""
-        # check
-        if not main_flow_out.dtype == self.main_flow_type_out:
-            raise TypeError(f"{main_flow_out.dtype} != {self.main_flow_type_out}")
-
         # get parameters data
         eff = ptxboa.classes._generated.PtxboaParameterTypes.EFF.create(
             get_data=get_data
@@ -115,34 +117,44 @@ class PtxboaProcessType(PtxboaTypeBase):
 
 
 @dataclass(frozen=True, slots=True)
-class PtxboaProcessTree(PtxboaProcessType):
+class PtxboaProcessTree(PtxboaProcess):
     """Parameter instance variable."""
 
     dtype: "PtxboaProcessType"
-    eff: PtxboaParameter
+    eff: PtxboaParameter = field(init=False)
     main_flow_out: "PtxboaFlow" = field(init=False)
     main_flow_in: "PtxboaFlow" = field(init=False)
 
-    processes: tuple[PtxboaProcess, ...]
+    _processes: tuple[PtxboaProcess, ...]
 
     def __post_init__(self):
 
         # TODO: this might not work if its an actual graph
-        first_process = self.processes[0]  # noqa
-        last_process = self.processes[-1]  # noqa
-
-        main_flow_in = self.dtype.main_flow_type_in.create(
-            self.main_flow_out.value / self.eff.value
-        )
+        first_process = self._processes[0]  # noqa
+        last_process = self._processes[-1]  # noqa
 
         object.__setattr__(
             self,
             "main_flow_in",
-            main_flow_in,
+            first_process.main_flow_in,
+        )
+        object.__setattr__(
+            self,
+            "main_flow_out",
+            last_process.main_flow_out,
+        )
+        object.__setattr__(
+            self,
+            "eff",
+            PtxboaParameter(
+                dtype=ptxboa.classes._generated.PtxboaParameterTypes.EFF,
+                value=last_process.main_flow_out.value
+                / first_process.main_flow_in.value,
+            ),
         )
 
     def __str__(self) -> str:
-        return f"{self.code}({', '.join(str(x) for x in self.processes)})"
+        return f"{self.code}({', '.join(str(x) for x in self._processes)})"
 
 
 @dataclass(frozen=True, slots=True)
@@ -179,22 +191,18 @@ class PtxboaProcessTreeType(PtxboaProcessType):
         # IMPORTANT: initialize backwards
 
         current_main_out: PtxboaFlow = main_flow_out
+        processes = []
         for process_type in reversed(self.process_types):
             process = process_type.create(
                 get_data=get_data, main_flow_out=current_main_out
             )
+            processes.append(process)
             current_main_out = process.main_flow_in
 
-        main_flow_in = process.main_flow_in
-
-        # calculated eff
-
-        eff = PtxboaParameter(
-            dtype=ptxboa.classes._generated.PtxboaParameterTypes.EFF,
-            value=main_flow_out.value / main_flow_in.value,
+        return PtxboaProcessTree(
+            dtype=self,
+            _processes=tuple(reversed(processes)),
         )
-
-        return PtxboaProcessTree(dtype=self, eff=eff, main_flow_out=main_flow_out)
 
 
 @dataclass(frozen=True, slots=True)
